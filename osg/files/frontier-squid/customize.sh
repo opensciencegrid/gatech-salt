e321b8ce12bd27bb567004572b38a6249dad9efd
Suchandra Thapa <sthapa@ci.uchicago.edu>
2018-08-22 16:19:09 -0500

    Fix squid customize

--- osg/files/frontier-squid/customize.sh
+++ osg/files/frontier-squid/customize.sh
@@ -1,3 +1,8 @@
+awk --file `dirname $0`/customhelps.awk --source '{
 setoption("acl NET_LOCAL src", "130.207.168.0/24 172.26.79.0/24")
-
+setoption("cache_mem", "256 MB")
 setoptionparameter("cache_dir", 3, "10000")  # 10GB
+setoptionparameter("cache_dir", 1, "aufs")
+print
+}'
+
